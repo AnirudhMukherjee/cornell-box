addea3de94410c4917ccae482192c8869db1519f
AnirudhMukherjee <anirudhmk130@gmail.com>
2020-10-07 11:27:40 +0530

    init

---
+++ Assets/Sketch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour
+{
+
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int totalCubes = 14;
+        float totalDistance = 2f;
+
+        for (int i = 0; i < totalCubes; i++)
+        {
+            float perc = (float)i / (float)totalCubes;
+            float sin = Mathf.Sin(perc*Mathf.PI/2);
+            float x = 1.5f+ sin * totalDistance;
+            float y = 6f;
+            float z = 0.0f;
+            var newCube = (GameObject)Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<Spin>().SetSize(0.5f* (1.0f-perc));
+            newCube.GetComponent<Spin>().rotateSpeed = 0.2f+ perc*1.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Spin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float rotateSpeed = 1.0f;
+    public Vector3 spin = Vector3.zero;
+    public float size1 = 1.5f;
+    public Vector3 spinAxis = new Vector3(0, 1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SetSize(size);
+        //transform.position = new Vector3(0, 5, 3);
+        spin = new Vector3(Random.value, Random.value, Random.value);
+        spinAxis.x = Random.Range(-1, 1)*.1f;
+    }
+
+    public void SetSize(float size)
+    {
+        this.transform.localScale = new Vector3(size, size, size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(spin);
+        this.transform.RotateAround(Vector3.zero, spinAxis, rotateSpeed);
+    }
+}
